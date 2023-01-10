4df08311c8c53a838e74d44237cb84ee2d71c48b
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-10 12:31:41 +0100

    done 01 && 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(strings, repeats) {
+    var ConnectedString = ""
+    if(repeats < 0){
+        return"ERROR"
+    }
+    for(var i=0; i<repeats; i++){
+        ConnectedString = ConnectedString + strings
+    }
+    return ConnectedString
 };
 
 // Do not edit below this line

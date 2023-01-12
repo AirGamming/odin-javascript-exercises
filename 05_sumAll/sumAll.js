201871feaf8936ce71465e57bcaa8c308ad56d5e
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-12 20:06:25 +0100

    done 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    let smallint 
+    let bigint 
+    if(typeof(end) != "number"||typeof(start) != "number" ||
+    start < 0 || end < 0){
+        return "ERROR"
+    }else if(start < end){
+        smallint = start
+        bigint = end
+    }else{
+        smallint = end
+        bigint = start
+    }
+    for(let i = smallint; i <= bigint; i++){
+        sum = sum + i
+    }
+    return sum
 };
 
 // Do not edit below this line

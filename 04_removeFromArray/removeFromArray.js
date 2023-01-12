fefdcf1ede511b28f5784c1244504fec3cfdb26c
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-12 14:05:23 +0100

    done 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rmItems) {
+    
+    rmItems.forEach(el => {
+        
+        let indexRmItem = arr.indexOf(el);
+        if(indexRmItem == -1){}
+        else{
+        arr.splice(indexRmItem,1) 
+        }
+    });
+    return arr   
 };
 
 // Do not edit below this line

9bcd449f2d4698829572960264ed48415855cb57
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-13 22:38:00 +0100

    did 07 and 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  if(!typeof(number) == "number"){
+    return "ERROR"
+  }else{ 
+    let celcius = Math.round(((number - 32) * 0.5556) * 10) / 10
+    return celcius
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  if(!typeof(number) == "number"){
+    return "ERROR"
+  }else{ 
+    let celcius = Math.round(((number * 1.8) + 32) * 10) / 10
+    return celcius
+  }
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let equasion = 0
+  arr.forEach(element => {
+    equasion = equasion + element
+  });
+  return equasion
+}
+
+const multiply = function(arr) {
+  let equasion = 1
+  arr.forEach(el => {
+    equasion =equasion*el
+  });
+  return equasion
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return Math.pow(num1,num2)
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let equasion = 1
+  if(num == 0){
+    return 1
+  }
+  for(i=1; i<=num;i++){
+    equasion = equasion * i
+  }
+  return equasion;
 };
 
 // Do not edit below this line

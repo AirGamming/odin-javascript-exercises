1b19df3c3869bee41384577f09c2d34237ba8be9
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-14 13:40:04 +0100

    did 09,10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    let n1 = 0, n2 = 1, nextTerm;    
+    int = Number(int);
+    if(int < 0){
+        return"OOPS"
+    }else if(int == 1){
+        return 1
+    }
+    
+    for (let i = 1; i <= int-1; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return nextTerm
 };
 
 // Do not edit below this line

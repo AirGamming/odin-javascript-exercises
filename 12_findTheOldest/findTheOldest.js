5c20e2d4a56440d55fd6a6673da8e9470e2da77e
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-15 01:45:33 +0100

    did 13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (arr) {
+    let oldestName ={};
+    let oldest = 0;
+    arr.forEach((el) => {
+      age = el.yearOfDeath - el.yearOfBirth
+      let currentYear = new Date().getFullYear();
+      if (
+        el.hasOwnProperty('yearOfDeath') && el.yearOfDeath - el.yearOfBirth >= oldest
+      ) {
+        oldest = el.yearOfDeath - el.yearOfBirth;
+        oldestName = el;
+        console.log(oldest)
+      } else if (currentYear - el.yearOfBirth > oldest && !el.hasOwnProperty('yearOfDeath') ) {
+        oldest = (currentYear - el.yearOfBirth)
+        oldestName = el;
+      }
+    });
+    console.log(oldest)
+    return oldestName;
+  };
 // Do not edit below this line
 module.exports = findTheOldest;
